e95458c401ec6803bd19f0bf7d238568e2c3c747
Inom T. <i@inomoz.ru>
2025-12-08 20:49:12 +0300

    Add types with pydantic modeles

---
+++ data_types.py
@@ -0,0 +1,43 @@
+from typing import Annotated, TypedDict
+from fastapi import Query
+from pydantic_extra_types.phone_numbers import PhoneNumber
+from redis.asyncio import Redis
+from pydantic import BaseModel
+
+
+class State(TypedDict):
+    client: Redis
+
+
+class RUPhone(PhoneNumber):
+    default_region_code = "RU"
+    supported_regions = ["RU"]
+
+
+Phone = Annotated[RUPhone, Query(example="tel:+7-999-999-99-99")]
+
+
+# Potentially we can use smarter model, maybe even some API to verify that
+# model, https://dadata.ru/
+class HomeAddress(BaseModel):
+    street: str
+    apartment_number: str
+    city: str
+    state_province: str
+    postal_code: str
+    country: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "street": "123 Main St",
+                    "apartment_number": "Apt 4B",
+                    "city": "Anytown",
+                    "state_province": "CA",
+                    "postal_code": "90210",
+                    "country": "USA",
+                }
+            ]
+        }
+    }
